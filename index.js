5200a037c0cde9d5bd2563a963a21190775fa37f
SUC <suc.yannick@gmail.com>
2021-03-27 00:07:19 +0100

    Change port to dynamic

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(app.oauth.errorHandler());
 app.use("/auth", routes);
 app.use("/test", testAPIRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
