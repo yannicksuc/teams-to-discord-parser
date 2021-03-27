34d361cf487f801e15b36f37d3a5c86e3f07339f
SUC <suc.yannick@gmail.com>
2021-03-27 17:52:49 +0100

    Add html to markdown parser

--- db/pgWrapper.js
+++ db/pgWrapper.js
@@ -10,10 +10,7 @@ function query(queryString, cbFunc) {
 		host: process.env.DB_HOST,
 		database: process.env.DB_NAME,
 		password: process.env.DB_PASSWORD,
-		port: parseInt(process.env.DB_PORT),
-		ssl: {
-			rejectUnauthorized: false
-		}
+		port: parseInt(process.env.DB_PORT)
 	});
 
 	pool.query(queryString, (error, results) => {
--- test/testAPIService.js
+++ test/testAPIService.js
@@ -3,7 +3,34 @@ module.exports = {
 };
 
 function helloWorld(req, res) {
-	res.send("Hello World OAuth2!");
+	const fs = require('fs')
+	try {
+		var data = req.content;
+		var h2m = require('h2m')
+		var md = h2m(data, {
+			overides: {
+				li: function (node) {
+					if (node.md) {
+						return ` • ${node.md}\n`
+					}
+				},
+				s: function (node) {
+					if (node.md) {
+						return `~~${node.md}~~`
+					}
+				}
+			}
+		})
+		md = md.replace(/\\t(?!•)(.+\n)/g, "   $1")
+		md = md.replace(/\n\\t/g, '')
+		md = md.replace(/\n\n/g, '\n')
+		md = md.replace(/\\t/g, '')
+		md = md.replace(/ +•/g, " •")
+		md = md.replace(/\!\[(.+?)\]\(https\:\/\/statics\.teams\.cdn\.office\.net\/evergreen\-assets\/skype\/v2\/[a-zA-Z\-_]+\/20\.png\)/g, "$1")
+		res.send(md);
+	} catch (err) {
+		console.error(err)
+	}
 }
 
 function sendBack(req, res) {
